3d966ffe30bc8e56606d8622d3f06df0a5ecdf4a
balex88 <aleber88@gmail.com>
2019-04-25 20:44:29 -0400

    Update BookList.vue

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,13 +1,21 @@
 <template>
   <div id="BookList">
     <h1>{{title}}</h1>
+    <ul>
+      <li v-for="book in books" :key="book.title">{{book.title}} : {{book.author}}</li>
+  </ul>
   </div>
 </template>
 <script>
 export default {
   name: "BookList",
   data: () => ({
-    title: 'All Books'
+    title: 'All Books',
+    books: [
+      {title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
+      {title: 'American Gods', author: 'Neil Gaiman'},
+      {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
+    ]
   })
 }
 </script>
@@ -15,4 +23,6 @@ export default {
 h1, h2 {
   font-weight: normal;
 }
+ul { list-style-type: none; padding: 0; }
+li {display: block; margin: 0 10px;}
 </style>
