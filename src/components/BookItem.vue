daad6ebaa3212c56a2a66d4c08956166426998d8
balex88 <aleber88@gmail.com>
2019-04-25 20:48:26 -0400

    Add BookItem Component

---
+++ src/components/BookItem.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="BookItem">
+    <li>{{book.title}} : {{book.author}}</li>
+  </div>
+</template>
+<script>
+export default {
+  name: "BookItem",
+  props: ['book']
+}
+</script>
+<style lang="scss" scoped>
+  li {display: block; margin: 0 10px;}
+</style>
